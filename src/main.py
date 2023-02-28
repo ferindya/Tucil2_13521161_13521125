b1072badf0f6e55d1a53c6002a5d5ba8c18148e8
unknown <ferindyaulia19@gmail.com>
2023-02-28 21:55:03 +0700

    update src

--- src/main.py
+++ src/main.py
@@ -1,23 +1,23 @@
 import math
 import random
-import time
+from time import time
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 
 # Algoritma Brute Force untuk mencari pasangan titik terdekat
 def findDistanceBruteForce(points):
     n = len(points)
-    min_dist = float('inf')
-    closest_pair = None
+    minDistance = float('inf')
+    closestPair = None
     
     for i in range(n):
         for j in range(i+1, n):
             d = findDistance(points[i], points[j])
-            if d < min_dist:
-                min_dist = d
-                closest_pair = (points[i], points[j])
+            if d < minDistance:
+                minDistance = d
+                closestPair = (points[i], points[j])
     
-    return closest_pair, min_dist
+    return closestPair, minDistance
 
 # Algoritma Divide and Conquer untuk mencari pasangan titik terdekat
 def findDistanceDivideConquer(points):
@@ -34,73 +34,89 @@ def findDistanceDivideConquer(points):
     right = points[mid:]
     
     # Mencari pasangan terdekat di setiap bagian secara rekursif
-    left_pair, left_dist = findDistanceDivideConquer(left)
-    right_pair, right_dist = findDistanceDivideConquer(right)
+    leftPair, leftDistance = findDistanceDivideConquer(left)
+    rightPair, rightDistance = findDistanceDivideConquer(right)
     
     # Mencari pasangan titik terdekat di kedua bagian
-    if left_dist < right_dist:
-        closest_pair = left_pair
-        min_dist = left_dist
+    if leftDistance < rightDistance:
+        closestPair = leftPair
+        minDistance = leftDistance
     else:
-        closest_pair = right_pair
-        min_dist = right_dist
+        closestPair = rightPair
+        minDistance = rightDistance
     
     # Mencari pasangan titik terdekat yang melintasi garis tengah
     strip = []
     for point in points:
-        if abs(point[0] - points[mid][0]) < min_dist:
+        if abs(point[0] - points[mid][0]) < minDistance:
             strip.append(point)
     strip = sorted(strip, key=lambda x: x[1])
     
     for i in range(len(strip)):
         for j in range(i+1, len(strip)):
-            if strip[j][1] - strip[i][1] >= min_dist:
+            if strip[j][1] - strip[i][1] >= minDistance:
                 break
             d = findDistance(strip[i], strip[j])
-            if d < min_dist:
-                min_dist = d
-                closest_pair = (strip[i], strip[j])
+            if d < minDistance:
+                minDistance = d
+                closestPair = (strip[i], strip[j])
     
-    return closest_pair, min_dist
+    return closestPair, minDistance
 
 # Fungsi untuk menghitung jarak antara 2 titik dalam ruang 3D
 def findDistance(p1, p2):
-    return math.sqrt((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2 + (p1[2]-p2[2])**2)
+    return math.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2 + (p1[2] - p2[2])**2)
 
-# Generate secara random 10 titik dalam ruang 3D
+# ASCII ART
+print("       \:.             .:/                                                       ")
+print("        \``._________.''/                                                        ")
+print("         \             /                                                         ")  
+print(" .--.--, / .':.   .':. \                                                         ")
+print("/__:  /  | '::' . '::' |        === WELCOME TO CLOSEST PAIR OF POINTS PROGRAM ===")
+print("   / /   |`.   ._.   .'|                       BY 13521161 & 13521125            ")
+print("  / /    |.'         '.|                                                         ")
+print(" /___-_-,|.\  \   /  /.|                                                         ")
+print("      // |''\.;   ;,/ '|                                                         ")
+print("      `==|:=         =:|                                                         ")
+print("         `.          .'                                                          ")
+print("           :-._____.-:                                                           ")
+print("          `''       `''                                                          ")
+print("                                                                                 ")
+
+# Generate secara random 100 titik dalam ruang 3D
 num_points = int(input("Masukkan jumlah titik pada ruang 3D (n) : "))
-points = [(random.uniform(-10, 10), random.uniform(-10, 10), random.uniform(-10, 10)) for i in range(num_points)]
+points = [(random.uniform(-100, 100), random.uniform(-100, 100), random.uniform(-100, 100)) for i in range(num_points)]
 
 # Mencari pasangan titik terdekat menggunakan algoritma Brute Force
-start_time = time.time()
-bf_pair, bf_dist = findDistanceBruteForce(points)
-bf_time = (time.time() - start_time) * 100
-bf_ops = num_points**2/2
+startTime = time()
+bruteForcePair, bruteForceDistance = findDistanceBruteForce(points)
+bruteForceTime = (time() - startTime) 
+bruteForceOp = num_points**2/2
 
 # Mencari pasangan titik terdekat menggunakan algoritma Divide and Conquer
-start_time = time.time()
-dc_pair, dc_dist = findDistanceDivideConquer(points)
-dc_time = (time.time() - start_time) * 100
-dc_ops = num_points * math.log(num_points, 2)
+startTime = time()
+divideConquerPair, divideConquerDistance = findDistanceDivideConquer(points)
+divideConquerTime = (time() - startTime)
+divideConquerOp = num_points * math.log(num_points, 2)
 
 # Mencetak pasangan titik terdekat beserta nilai jarak untuk algoritma brute force dan divide and conquer
-print("---------------------[[ALGORITMA BRUTE FORCE]]---------------------")
-print("Pasangan titik terdekat : ", bf_pair)
-print("Nilai jarak : ", bf_dist)
-print("Banyaknya operasi perhitungan rumus Euclidean : ", bf_ops)
-print("Waktu Eksekusi : ", bf_time)
-
-print("---------------------[[ALGORITMA DIVIDE AND CONQUER]]---------------------")
-print("Pasangan titik terdekat : ", dc_pair)
-print("Nilai jarak :", dc_dist)
-print("Banyaknya operasi perhitungan rumus Euclidean : ", dc_ops)
-print("Waktu Eksekusi : ", dc_time)
+print("-----------------------------------------------------------------[[ALGORITMA BRUTE FORCE]]----------------------------------------------------------------")
+print("Pasangan titik terdekat : ", bruteForcePair)
+print("Nilai jarak : ", bruteForceDistance)
+print("Banyaknya operasi perhitungan rumus Euclidean : ", bruteForceOp)
+print("Waktu Eksekusi : ", bruteForceTime, "detik")
+print("                                                                                                                                                          ")
+print("-------------------------------------------------------------[[ALGORITMA DIVIDE AND CONQUER]]-------------------------------------------------------------")
+print("Pasangan titik terdekat : ", divideConquerPair)
+print("Nilai jarak :", divideConquerDistance)
+print("Banyaknya operasi perhitungan rumus Euclidean : ", divideConquerOp)
+print("Waktu Eksekusi : ", divideConquerTime, "detik")
 
 # Visualisasi
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 ax.scatter([p[0] for p in points], [p[1] for p in points], [p[2] for p in points], s=10)
-ax.scatter([dc_pair[0][0], dc_pair[1][0]], [dc_pair[0][1], dc_pair[1][1]], [dc_pair[0][2], dc_pair[1][2]], s=50, c='r')
+ax.scatter([divideConquerPair[0][0], divideConquerPair[1][0]], [divideConquerPair[0][1], divideConquerPair[1][1]], [divideConquerPair[0][2], divideConquerPair[1][2]], s=50, c='r')
 ax.set_xlabel('Sumbu X')
 ax.set_ylabel('Sumbu Y')
 ax.set_zlabel('Sumbu Z')
